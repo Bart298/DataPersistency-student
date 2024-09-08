0e522e95d2111f6e1580979fe38a3fb4e52f3bf0
Bart298 <bart.klomp@student.hu.nl>
2024-09-08 23:23:58 +0200

    S1 uitgewerkt

--- opdracht/S1_DDL_student.sql
+++ opdracht/S1_DDL_student.sql
@@ -35,6 +35,8 @@
 -- geaccepteerd. Test deze regel en neem de gegooide foutmelding op als
 -- commentaar in de uitwerking.
 
+ALTER TABLE medewerkers ADD geslacht CHAR CONSTRAINT m_geslacht_chk CHECK (geslacht IN ('M', 'V')) 
+
 
 -- S1.2. Nieuwe afdeling
 --
@@ -43,6 +45,17 @@
 -- nieuwe medewerker A DONK aangenomen. Hij krijgt medewerkersnummer 8000
 -- en valt direct onder de directeur.
 -- Voeg de nieuwe afdeling en de nieuwe medewerker toe aan de database.
+
+
+INSERT INTO medewerkers (mnr, naam, voorl, functie, chef, gbdatum, maandsal, comm, afd)
+VALUES (8000, 'DONK', 'A', 'AFD_HOOFD', 7839, '17-12-1988', 800, NULL, NULL)
+
+INSERT INTO afdelingen (anr, naam, locatie, hoofd)
+VALUES (50, 'ONDERZOEK', 'ZWOLLE', 8000)
+
+UPDATE medewerkers
+SET afd = 50
+WHERE mnr = 8000;
 
 
 -- S1.3. Verbetering op afdelingentabel
@@ -54,6 +67,26 @@
 --      de nieuwe sequence.
 --   c) Op enig moment gaat het mis. De betreffende kolommen zijn te klein voor
 --      nummers van 3 cijfers. Los dit probleem op.
+
+--A 
+-- start staat op 60 omdat afdeling onderzoek nummer 50 heeft
+CREATE SEQUENCE afdeling_anr_seq
+INCREMENT 10
+START 60
+MINVALUE 10
+MAXVALUE 250
+CACHE 1;
+
+--B
+INSERT INTO afdelingen (anr, naam, locatie, hoofd)
+VALUES (nextval('afdeling_anr_seq'), 'MAGAZIJN', 'UTRECHT', 8000),
+       (nextval('afdeling_anr_seq'), 'MARKETING', 'EMMEN', 7698),
+       (nextval('afdeling_anr_seq'), 'INKOOP', 'RHENEN', 7902)
+
+
+
+--C
+ALTER TABLE afdelingen ALTER COLUMN anr TYPE NUMERIC(3);
 
 
 -- S1.4. Adressen
@@ -69,12 +102,27 @@
 --    telefoon      10 cijfers, uniek
 --    med_mnr       FK, verplicht
 
+CREATE TABLE adressen (
+    postcode CHAR(6) CONSTRAINT a_postcode_chk CHECK (regexp_like(postcode, '[0-9]{4}[A-Z]{2}')) NOT NULL,
+    huisnummer INT CONSTRAINT a_huisnummer NOT NULL,
+    ingangsdatum DATE CONSTRAINT a_ingangsdatum NOT NULL,
+    einddatum DATE CONSTRAINT a_beg_eind_chk CHECK (ingangsdatum < einddatum) NOT NULL,
+    telefoonnummer CHAR(10) CONSTRAINT a_telefoon_num UNIQUE,
+    med_nmr NUMERIC(4) CONSTRAINT a_med_nmr REFERENCES medewerkers NOT NULL,
+	CONSTRAINT adressen_pk PRIMARY KEY (postcode, huisnummer,ingangsdatum)
+)
+
+INSERT INTO adressen (postcode, huisnummer, ingangsdatum, einddatum, telefoonnummer, med_nmr)
+VALUES ('1234AB', 8, '2004-08-18', '2024-08-18', '0612345678', 8000)
 
 -- S1.5. Commissie
 --
 -- De commissie van een medewerker (kolom `comm`) moet een bedrag bevatten als de medewerker een functie als
 -- 'VERKOPER' heeft, anders moet de commissie NULL zijn. Schrijf hiervoor een beperkingsregel. Gebruik onderstaande
 -- 'illegale' INSERTs om je beperkingsregel te controleren.
+
+ALTER TABLE medewerkers ADD CONSTRAINT m_comm_constraint CHECK 
+((functie = 'VERKOPER' AND comm IS NOT NULL) OR (functie <> 'VERKOPER' AND comm IS NULL));
 
 INSERT INTO medewerkers (mnr, naam, voorl, functie, chef, gbdatum, maandsal, comm)
 VALUES (8001, 'MULLER', 'TJ', 'TRAINER', 7566, '1982-08-18', 2000, 500);
